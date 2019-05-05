8a834a34606c1a1766e52cbc9262817d26f282cd
Gabriel-Sa-Barreto <bielbarretoalves@gmail.com>
2019-05-05 14:49:33 -0300

    Impressão de sprite completa

--- Modulos/top.v
+++ Modulos/top.v
@@ -1,5 +1,7 @@
 module top(
-		input      clk,          //clock da FPGA (50MHz)
+	  input      clk,          //clock da FPGA (50MHz)
+	  input  wire leftSprite,	  //mover sprite para a esquerda
+	  input  wire rightSprite,    //mover sprite para a direita
 	  output reg  [2:0] VGA_R,   //intensidade de vermelho
 	  output reg  [2:0] VGA_G,   //intensidade de verde
 	  output reg  [2:0] VGA_B,   //intensidade do azul
@@ -11,6 +13,9 @@ module top(
 wire	 video_enable;    //sinal de area ativa da tela para impressao
 wire [10:0]	pixel_x;    //coordenada x do pixel atual da tela 
 wire [9:0]  pixel_y;    //coordenada y do pixel atual da tela
+
+reg [9:0] sprite_x;
+reg [9:0] sprite_y;
 
 
 SVGA_sync	SVGA(.clock(clk),
@@ -47,7 +52,7 @@ sram #(
         .ADDR_WIDTH(VRAM_A_WIDTH), 
         .DATA_WIDTH(VRAM_D_WIDTH), 
         .DEPTH(VRAM_DEPTH), 
-        .MEMFILE("/home/gabriel/Documentos/ConsoleFPGA/ConsoleFGA/teste2.mem"))  // bitmap to load
+        .MEMFILE("/home/gabriel/Documentos/ConsoleFPGA/ConsoleFGA/teste3.mem"))  // bitmap to load
         vram (
         .i_addr(address), 
         .i_clk(clk), 
@@ -60,23 +65,36 @@ reg [11:0] palette [0:255];  // 64 x 12-bit colour palette entries
 reg [11:0] colour;
 
 initial begin
-        $readmemh("/home/gabriel/Documentos/ConsoleFPGA/ConsoleFGA/teste2_palette.mem", palette);  // bitmap palette to load
+	sprite_x = 0;
+	sprite_y = 0;
+	address  = 0;
+   $readmemh("/home/gabriel/Documentos/ConsoleFPGA/ConsoleFGA/teste3_palette.mem", palette);  // bitmap palette to load
 end
 
 always @ (posedge clk)
     begin
-		  if(pixel_x == 0 && pixel_y == 0) address <= 0;
+		  address <= (SPRITE_SIZE * sprite_y) + sprite_x;
         if (video_enable)
 				begin
 					if( ( pixel_x >= 400 && pixel_x <= 432) && (pixel_y >= 300 && pixel_y <= 332)  )//caso esteja no centro
 						begin
 							colour <= palette[dataout];
-							address <= address + 1;
+							if(sprite_y > SPRITE_SIZE) 
+							begin
+								sprite_y <= 0;
+								sprite_x <= 0;
+							end
+							if(sprite_x > SPRITE_SIZE-1) 
+								begin
+									sprite_x <= 0;
+									sprite_y <= sprite_y + 1;
+								end
+							else sprite_x <= sprite_x + 1;
 						end
 					else
 						begin
 							colour[11:8] <= 0;
-							colour[7:4]  <= 1;
+							colour[7:4]  <= 0;
 							colour[3:0]  <= 0;
 						end
 				end
@@ -87,5 +105,6 @@ always @ (posedge clk)
         VGA_R <= colour[10:8];
         VGA_G <= colour[6:4];
         VGA_B <= colour[2:0];
-    end		
+    end			 
+		 
 endmodule
