80622b51efb268e81a0e92c3218e4065f65acf49
Gabriel-Sa-Barreto <bielbarretoalves@gmail.com>
2019-05-18 20:11:54 -0300

    armazenamento de diferentes sprites

--- Modulos/top.v
+++ Modulos/top.v
@@ -39,24 +39,29 @@ localparam SCREEN_HEIGHT = 600;
 //Definiçao do espaço que a imagem ira ocupar
 localparam SPRITE_SIZE = 32;
 ////////////////////////////////////////////
-//Número de itens que ira para a matriz de memoria
-localparam VRAM_DEPTH = SPRITE_SIZE * SPRITE_SIZE; 
+//Definiçao do numero de sprites armazenado na memoria
+localparam SPRITE_COUNT = 8;
 ////////////////////////////////////////////
-//// 2^10 e o valor correspondente a 32 x 32.
-localparam VRAM_A_WIDTH = 10;  
+//Número de itens que ira para a matriz de memoria
+localparam VRAM_DEPTH = SPRITE_SIZE * SPRITE_SIZE * SPRITE_COUNT; 
+////////////////////////////////////////////
+//// 2^13 e o valor correspondente a 32 x 32 x 8.
+localparam VRAM_A_WIDTH = 13;  
 ////////////////////////////////////////////
 //Quantidade de bits para a cor de um pixel
 localparam VRAM_D_WIDTH = 8;   
 ////////////////////////////////////////////
 reg [VRAM_A_WIDTH-1:0] address;      //endereco da memoria
 wire [VRAM_D_WIDTH-1:0] dataout;     //saida de dados da memoria
+///////////////////////////////////////////
+localparam SPRITE_OFFSET = 4 * SPRITE_SIZE * SPRITE_SIZE;
 
 
 sram #(
         .ADDR_WIDTH(VRAM_A_WIDTH), 
         .DATA_WIDTH(VRAM_D_WIDTH), 
         .DEPTH(VRAM_DEPTH), 
-        .MEMFILE("/home/gabriel/Documentos/ConsoleFPGA/ConsoleFGA/teste3.mem"))  // bitmap to load
+        .MEMFILE("/home/gabriel/Documentos/ConsoleFPGA/ConsoleFGA/sprites.mem"))  // bitmap to load
         vram (
         .i_addr(address), 
         .i_clk(clk), 
@@ -74,7 +79,7 @@ initial begin
 	address  = 0;
 	x_position = 400;
    y_position = 300;
-   $readmemh("/home/gabriel/Documentos/ConsoleFPGA/ConsoleFGA/teste3_palette.mem", palette);  // bitmap palette to load
+   $readmemh("/home/gabriel/Documentos/ConsoleFPGA/ConsoleFGA/sprites_palette.mem", palette);  // bitmap palette to load
 end
 
 always @ (posedge changePosition)
@@ -89,7 +94,7 @@ end
 
 always @ (posedge clk)
     begin
-		  address <= (SPRITE_SIZE * sprite_y) + sprite_x;
+		  address <= SPRITE_OFFSET + (SPRITE_SIZE * sprite_y) + sprite_x;
         if (video_enable)
 				begin
 					buildSprite(x_position,y_position);	
@@ -98,9 +103,9 @@ always @ (posedge clk)
 				begin 
 					colour <= 0;
 				end
-        VGA_R <= colour[10:8];
-        VGA_G <= colour[6:4];
-        VGA_B <= colour[2:0];
+        VGA_R <= colour[11:9];
+        VGA_G <= colour[7:5];
+        VGA_B <= colour[3:1];
     end			 
 		 
 task buildSprite;
@@ -115,12 +120,15 @@ task buildSprite;
 				sprite_y <= 0;
 				sprite_x <= 0;
 			end
-			if(sprite_x > SPRITE_SIZE-1) 
+			else
 			begin
-				sprite_x <= 0;
-				sprite_y <= sprite_y + 1;
+				if(sprite_x > SPRITE_SIZE-1) 
+				begin
+					sprite_x <= 0;
+					sprite_y <= sprite_y + 1;
+				end
+				else sprite_x <= sprite_x + 1;
 			end
-			else sprite_x <= sprite_x + 1;
 		end
 		else
 		begin
@@ -130,10 +138,5 @@ task buildSprite;
 		end
 	end
 endtask
-
-		 
-		 
-		 
-		 
 		 
 endmodule
